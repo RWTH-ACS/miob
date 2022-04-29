1d7751f7541a6beae431ea850e1bf02661c4bcbe
Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
2022-04-29 14:39:45 +0200

    add xsdb script to program fpga

---
+++ sw/hw_server/fpga-program.tcl
@@ -0,0 +1,23 @@
+# Program the FPGA via a hw_server
+
+if { $argc != 2 } {
+	puts "Wrong number of arguments."
+        puts "Usage: $argv0 [hw_server ip] [fpga bitstream file]"
+} else {
+	puts "connection to [lindex $argv 0]"
+	if { [ catch {connect -host [lindex $argv 0]} err] } {
+		puts "Error connecting: $err"
+		exit 1
+	}
+	if { [ catch {targets -set -filter {name == "PL"}} err] } {
+		puts "Error setting target: $err"
+		exit 1
+	}
+	puts "programming [lindex $argv 1]"
+	if { [ catch {fpga [lindex $argv 1]} err] } {
+		puts "Error programming bitstream: $err"
+		exit 1
+	}
+	puts "[fpga -state]"
+}
+
