46826ec6a9c4699a637c85967c5c68d875097935
Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
2024-02-08 10:58:44 +0100

    fpga/zcu106_aurora_minimal: update design
    Signed-off-by: Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>

--- fpga/zcu106_aurora_minimal/aurora_demo.vhd
+++ fpga/zcu106_aurora_minimal/aurora_demo.vhd
@@ -35,16 +35,17 @@ use IEEE.NUMERIC_STD.ALL;
 entity aurora_demo is
     Port ( clk : in STD_LOGIC;
            reset : out STD_LOGIC;
-           m_axis_tdata : out STD_LOGIC_VECTOR (31 downto 0);
-           m_axis_tkeep : out STD_LOGIC_VECTOR (1 downto 0);
+           m_axis_tdata : out STD_LOGIC_VECTOR (15 downto 0);
+           --m_axis_tkeep : out STD_LOGIC_VECTOR (1 downto 0);
            m_axis_tlast : out STD_LOGIC;
-           m_axis_tready : in STD_LOGIC;
+           --m_axis_tready : in STD_LOGIC;
            m_axis_tvalid : out STD_LOGIC);
 end aurora_demo;
 
 architecture Behavioral of aurora_demo is
 	signal timestep_begin : STD_LOGIC := '0';
     signal clk_cnt : integer := 0;
+    signal voltage_i : integer := 0;
     signal payload_id : integer := 0;
     signal do_reset : std_logic := '1';
     
@@ -52,17 +53,17 @@ architecture Behavioral of aurora_demo is
     --constant clock_frequency : integer := 100e6;    
     constant clock_frequency : integer := 50e6;    
     
-    constant voltage1 : integer := 1;
-    constant voltage2 : integer := 120;
-    constant voltage3 : integer := 240;
+    constant voltage1 : integer := 120;
+    constant voltage2 : integer := 240;
+    constant voltage3 : integer := 1;
 begin
 
-m_axis_tkeep <= (others => '1');
+--m_axis_tkeep <= (others => '1');
 reset <= do_reset;
 
-process (clk, clk_cnt, payload_id, timestep_begin, m_axis_tready) begin
+process (clk, clk_cnt, payload_id, timestep_begin) begin
     if rising_edge(clk) then
-            if timestep_begin = '1' and m_axis_tready = '0' then
+            if timestep_begin = '1' then --and m_axis_tready = '0' then
                 clk_cnt <= clk_cnt;
             else 
                 clk_cnt <= clk_cnt + 1;
@@ -77,25 +78,21 @@ process (clk, clk_cnt, payload_id, timestep_begin, m_axis_tready) begin
             if timestep_begin = '1' then
                 case clk_cnt is
                     when 0 =>
-                        m_axis_tdata <= std_logic_vector(to_unsigned(payload_id, 32));
-                        payload_id <= (payload_id + 1) mod 65536;
+                        voltage_i <= voltage_i + 1;
+                        if (voltage_i mod 3 = 0) then
+                            m_axis_tdata <= std_logic_vector(to_unsigned(voltage1, 16));
+                        elsif (voltage_i mod 3 = 1) then
+                            m_axis_tdata <= std_logic_vector(to_unsigned(voltage2, 16));
+                        elsif (voltage_i mod 3 = 2) then
+                            m_axis_tdata <= std_logic_vector(to_unsigned(voltage3, 16));
+                        end if;                   
                         m_axis_tvalid <= '1';
-                    when 1 =>
-                        m_axis_tdata <= std_logic_vector(to_unsigned(voltage1, 32));                    
-                        m_axis_tvalid <= '1';
-                    when 2 =>
-                        m_axis_tdata <= std_logic_vector(to_unsigned(voltage2, 32));   
-                        m_axis_tvalid <= '1';                
-                    when 3 =>
-                        m_axis_tdata <= std_logic_vector(to_unsigned(voltage3, 32));      
-                        m_axis_tvalid <= '1';   
                         m_axis_tlast <= '1';
-                    when 4 =>
+                    when others =>
                         m_axis_tdata <= (others => '0');
                         m_axis_tvalid <= '0';
                         m_axis_tlast <= '0';
                         timestep_begin <= '0';
-                    when others =>
                 end case;
             end if;
         end if;
--- fpga/zcu106_aurora_minimal/constr.xdc
+++ fpga/zcu106_aurora_minimal/constr.xdc
@@ -5,7 +5,7 @@ set_property PACKAGE_PIN W9 [get_ports GT_DIFF_REFCLK1_0_clk_n]
 set_property PACKAGE_PIN W10 [get_ports GT_DIFF_REFCLK1_0_clk_p]
 
 
-set_property LOC GTHE4_CHANNEL_X0Y10 [get_cells {design_1_i/aurora_8b10b_0/U0/design_1_aurora_8b10b_0_0_core_i/gt_wrapper_i/design_1_aurora_8b10b_0_0_gt_i/inst/gen_gtwizard_gthe4_top.design_1_aurora_8b10b_0_0_gt_gtwizard_gthe4_inst/gen_gtwizard_gthe4.gen_channel_container[2].gen_enabled_channel.gthe4_channel_wrapper_inst/channel_inst/gthe4_channel_gen.gen_gthe4_channel_inst[0].GTHE4_CHANNEL_PRIM_INST}]
+#set_property LOC GTHE4_CHANNEL_X0Y10 [get_cells {design_1_i/aurora_8b10b_0/U0/design_1_aurora_8b10b_0_0_core_i/gt_wrapper_i/design_1_aurora_8b10b_0_0_gt_i/inst/gen_gtwizard_gthe4_top.design_1_aurora_8b10b_0_0_gt_gtwizard_gthe4_inst/gen_gtwizard_gthe4.gen_channel_container[2].gen_enabled_channel.gthe4_channel_wrapper_inst/channel_inst/gthe4_channel_gen.gen_gthe4_channel_inst[0].GTHE4_CHANNEL_PRIM_INST}]
 set_property PACKAGE_PIN AA2 [get_ports {GT_SERIAL_RX_0_rxp[0]}]
 set_property PACKAGE_PIN AA1 [get_ports {GT_SERIAL_RX_0_rxn[0]}]
 set_property PACKAGE_PIN Y4 [get_ports {GT_SERIAL_TX_0_txp[0]}]
